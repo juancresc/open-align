36dad0a1acf0f66dad6bc89a2acf2218247d6ac2
Juan Manuel Crescente <juan.crescente@tiendanube.com>
2025-08-27 19:40:22 -0300

    remove gui

--- src/open_align/ui/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-from PyQt6.QtCore import QSize
-
-THUMB_SIZE = QSize(200, 200)
-SUPPORTED_SUFFIXES = {".png", ".jpg", ".jpeg", ".webp", ".bmp", ".gif", ".tif", ".tiff", ".heic"}
-
-# Where the feature image will be loaded from on step 2
-FEATURE_IMAGE_PATH = "tmp/features.png"
